596fb539cc7ffda0c454312b46ad30d4f8092c14
tiumeher <tiumeher@fit.cvut.cz>
2022-07-07 19:58:40 +0200

    fixed cut-off, <br> context

--- app/simplifier.py
+++ app/simplifier.py
@@ -16,22 +16,19 @@ class Simplifier():
     def load_model_config(self, mode):
         return SimplifierModel(json.load(open(f'modules/configs/{mode}.json')))
 
-    def control_broken_response(self, answer):
-        splitted_answer = re.split("(?<=[.!?]) +", answer)
-        if splitted_answer[-1][-1] not in ('.', '?', '!'):
-            corrected_answer = ' '.join(splitted_answer[0:-1])
-            if corrected_answer == '' and corrected_answer != answer: corrected_answer = 'Sorry, I cut off the whole thing because the answer was too short...'
-        return corrected_answer
+    def control_broken_response(self, response):
+        splitted_response = re.split("(?<=[.!?]) +", response)
+        if splitted_response[-1][-1] not in ('.', '?', '!'):
+            corrected_response = ' '.join(splitted_response[0:-1])
+            return corrected_response if corrected_response != '' and corrected_response != response else 'Sorry, I cut off the whole thing because the answer was too short...'
+        return response
 
     def response(self, text, temperature_response, top_p, max_tokens, mode, regen, cut_off):
         seq_model = self.load_model_config(mode)
         answer = ''
         for i in range(regen + 1):
-            print(i)
             answer = seq_model.response(text, temperature_response, top_p, max_tokens)
-            print('Answer1: ', answer)
             if answer[-1] in ('.', '?', '!'): break
-        print('Answer2: ', answer)
         if cut_off: answer = self.control_broken_response(answer)
-        print('Answer3: ', answer)
+        print(answer)
         return answer
