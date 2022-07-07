da4ed7f290ef346a270e35d990f7be215e41d4a3
tiumeher <tiumeher@fit.cvut.cz>
2022-07-07 14:20:05 +0200

    Fixed port

--- app/main.py
+++ app/main.py
@@ -39,4 +39,4 @@ if __name__ == "__main__":
     # http://127.0.0.1:8000/docs
     # http://127.0.0.1:8000/redoc
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=80, log_level="info")
